6620bbae3783a38f5ddff6c4027a3fbd8b412f0e
Cris Giovanoni <giovanonicris@gmail.com>
2019-10-18 09:11:36 -0500

    added zillow_util

--- zillow_util.py
+++ zillow_util.py
@@ -10,8 +10,6 @@ from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTran
 # Model
 from sklearn.linear_model import LinearRegression
 
-# =========== WORKING ZILLOW FUNCTIONS ============== #
-
 def get_db_url(db):
     """
     Produces a url from env credentials
@@ -22,16 +20,14 @@ def get_db_url(db):
     """
     return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
 
-#######!!!!!!!!! check your selection on usetype
 def get_sql_zillow():
     """
-    Queries from zillow database with the following conditions:
-    - Single-unit properties
-    - 
+    Queries from zillow database with the following specs:
+    - Fields: Parcel ID, Calculated Finished Square Footage, No. of Bathrooms, No. of Bedrooms, Last Transaction Date, Property Value
     >> Input:
-    database
+    none
     << Output:
-    url
+    data frame
     """
     query = '''
     SELECT prop.parcelid, calculatedfinishedsquarefeet, bathroomcnt, bedroomcnt, transactiondate, taxvaluedollarcnt
@@ -46,6 +42,16 @@ def get_sql_zillow():
     return df
 
 def filter_zillow_baseline(df):
+    """
+    Filters zillow query for baseline model with the following conditions: 
+    - Fields: Parcel ID, Calculated Finished Square Footage, No. of Bathrooms, No. of Bedrooms, Last Transaction Date, Property Value
+    - Renames queried fields
+    - Removes NaN
+    >> Input:
+    data frame
+    << Output:
+    filtered data frame
+    """
     # Filter Transaction Dates by boolean mask and loc
     df.transactiondate = pd.to_datetime(df.transactiondate, format='%Y-%m-%d')
     filter_dates = (df.transactiondate >= '2017-05-01') & (df.transactiondate <= '2017-06-30') 
@@ -57,11 +63,34 @@ def filter_zillow_baseline(df):
     return df
 
 def split_my_data(X, y, train_ratio=0.7):
+    """
+    Queries from zillow database with the following specs:
+    - Fields: Parcel ID, Calculated Finished Square Footage, No. of Bathrooms, No. of Bedrooms, Last Transaction Date, Property Value
+    >> Input:
+    Data frame with X
+    Data frame with y
+    Ratio of resulting train data
+    << Output:
+    X_train
+    X_test
+    y_train
+    y_test
+    """
     # Use when X and y data frames are available, and split train and test for modeling
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_ratio, random_state=123)
     return X_train, X_test, y_train, y_test
 
 def standard_scaler(X_train, X_test):
+    """
+    Scales X_train and X_test using Standard Scaler
+    >> Input:
+    X_train
+    X_test
+    << Output:
+    scaled_X_train
+    scaled_X_test
+    standard_scaler object
+    """
     standard_scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
     # Scale Train Data and Convert to a Data Frame
     scaled_X_train = standard_scaler.transform(X_train)
@@ -72,6 +101,17 @@ def standard_scaler(X_train, X_test):
     return scaled_X_train, scaled_X_test, standard_scaler
 
 def generate_linear_model(scaled_X_df, y_df):
+    """
+    Generates linear model.
+    Use scaled X data and unscaled y data.
+    >> Input:
+    scaled_X_df
+    y_df
+    << Output:
+    linear model
+    intercept of the linear model
+    coefficients of the linear model
+    """
     lm = LinearRegression()
     lm.fit(scaled_X_df, y_df)
     lm_intercept = lm.intercept_
@@ -79,6 +119,14 @@ def generate_linear_model(scaled_X_df, y_df):
     return lm, lm_intercept, lm_coefficients
 
 def predict_on_test(lm, scaled_X_test):
+    """
+    Predict y based on scaled_X data
+    >> Input:
+    linear model
+    scaled_X_test_df
+    << Output:
+    predicted y, a.k.a. yhat
+    """
     yhat = lm.predict(scaled_X_test)
     return yhat
 
